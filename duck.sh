ef8f34d25abac978e841a9dea9a2c405e453899a
hama77 <mountasargharsalli@gmail.com>
2026-05-12 15:46:19 +0100

    Update DuckDNS script with new domain and token

--- duck.sh
+++ duck.sh
@@ -1,1 +1,2 @@
-echo url="https://www.duckdns.org/update?domains=wwbesttv&token=YOUR_TOKEN_HERE&ip=" | curl -k -o ~/duckdns.log -K -
+echo url="https://www.duckdns.org/update?domains=q01&token=2e7e1deb-6f99-463a-b34e-f860f1a4fc2d&ip=" | curl -k -o ~/duckdns.log -K -s
+
